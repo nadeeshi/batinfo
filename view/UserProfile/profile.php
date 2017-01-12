6f8f5e2aaa62d94766671a4fddbc229353ff182b
Ahmed Rooshan <mhaarush@gmail.com>
2017-01-12 16:38:49 +0530

    userpro
    fdf

--- view/UserProfile/profile.php
+++ view/UserProfile/profile.php
@@ -2,30 +2,29 @@
 <html lang="en">
 <head>
   <title>nav1</title>
-  <link href="../assets/bootstrap/bootstrap.css" rel="stylesheet">
-  <link href="../assets/css/navbar1n2.css" rel="stylesheet">
-  <link href="../assets/css/phpPages.css" rel="stylesheet">
-  <link href="../assets/css/footer.css" rel="stylesheet">
-  <script src="../assets/bootstrap/jquary.js"></script> 
-  <script src="../assets/bootstrap/bootstrapjs.js"></script> 
-  <script src="../js/jssor.slider.mini.js"></script>
+  <link href="css/bootstrap.css" rel="stylesheet">
+ <link href="../../assets/css/navbar1n2.css" rel="stylesheet" type="text/css">
+  <link href="css/footer.css" rel="stylesheet">
+  <script src="js/jquary.js"></script>
+  <script src="js/bootstrapjs.js"></script>
 
    
     
 </head>
 
+
 <body>
 
     <div>
-        <?php include '../IncludedFiles/navbarTemplate.php'; ?>
+        <?php include '../../Assets/IncludedFiles/navbarTemplate.php'; ?>
     </div>
 
     <div class="container-fluid" style="padding-top:120px;">
     <div class="col-sm-8 col-sm-push-4 col-xs-12">
-	   <h2> Your Profile  </h2>
+	   <h2> Your Profile</h2>
        <br>
        <?php
-        include_once '../mainhome/dbconnect.php';
+        include_once '../../database/dbconnect.php';
         $id= $_GET['id'];
         $sql = "select * from researchers where id= $id";
         $res = $con->query($sql);
@@ -33,6 +32,7 @@
         while ($row = $res->fetch_assoc()) {
             ?>
          <div id = "info">
+         <form method="POST" action="update.php">
             <table border="0" class="table table-hover">
                 <tr>
                     <td>Profile ID</td>
@@ -40,11 +40,11 @@
                 </tr>
                 <tr>
                     <td>First Name</td>
-                    <td><input type="text" name="profileid" value="<?php echo $row['fname'];?>"></td>
+                    <td><input type="text" name="fname" value="<?php echo $row['fname'];?>"></td>
                 </tr>
                 <tr>
                     <td>Last Name</td>
-                    <td><input type="text" name="profileid" value="<?php echo $row['lname'];?>"></td>
+                    <td><input type="text" name="lname" value="<?php echo $row['lname'];?>"></td>
                 </tr>
                 <tr>
                     <td>NIC Number</td>
@@ -60,9 +60,13 @@
                 </tr>
                 <tr>
                     <td>Email</td>
-                    <td><input type="text" name="profileid" value="<?php echo $row['email'];?>" disabled=""></td>
+                    <td><input type="text" name="email" value="<?php echo $row['email'];?>"></td>
+                </tr>
+                <tr>
+                  <td colspan="2"><input type="submit" name="submit" value="Update Profile"></td>
                 </tr>
             </table>
+          </form>
        </div>       
         <?php    
         }
