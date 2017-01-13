6c3b9749ee22c29ef3800e2e4b546f4bd9b8a260
sewmi <sewmirupasinghe@gmail.com>
2017-01-13 23:09:59 +0530

    done with ui

--- assets/includedFiles/footer.php
+++ assets/includedFiles/footer.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-xs-12 footer-container">	
-		<div class="col-xs-push-4 col-xs-8 footcon-col-1">
+		<div class="col-xs-12 footcon-col-1">
 			<p>All Rights Reserved</p>
 		</div>
 	</div>
--- view/news/u_newst.php
+++ view/news/u_newst.php
@@ -13,10 +13,15 @@
 	
 </head>
 <body>
+	<div class="public-background">
+		<img src="../../assets/images/bat.jpg" width="100%" height="100%" >
+	</div>
 	<div>
 		<?php 
 		include ("../../assets/IncludedFiles/mainnav.php"); ?>
 	</div>
+	<div class="col-xs-12 body-content">
+	<div class="public-thread-content public-div-content col-xs-10">
 	<div class="col-sm-12 col-xs-12 insert-form">
 	<div class="container">
 	
@@ -213,7 +218,9 @@
 				</div>
 		</div>
 
-	</div>
+</div>
+</div>
+</div>
 	<!-- start footer -->
 
 	
