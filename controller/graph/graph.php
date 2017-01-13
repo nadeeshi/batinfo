f63a0830886a71a0b52f18815e18a949da5b807e
sewmi <sewmirupasinghe@gmail.com>
2017-01-13 11:36:45 +0530

    navbar added

--- controller/graph/graph.php
+++ controller/graph/graph.php
@@ -18,19 +18,22 @@
 
 <?php include "../../assets/includedFiles/navbarTemplate.php" ?>
 
-<div id="page-wrapper" class="col-sm-10 col-sm-push-2 col-xs-12 insert-form">
+<div id="page-wrapper" class="col-sm-10 col-sm-push-2 col-xs-12 insert-form insert-form-graph">
     <div id="page-inner" class="graph-content">
         <div class="row">
-            <div class="col-xs-12">
-                <p class="page-head-line">Welcome to BatsInfo</p>
-                <p class="page-subhead-line">Select Graph Type:- <a href="">Bar Chart</a>......<a href="pie_graph.php">Pie chart</a></p>
+            <div class="col-xs-10 page-head-graph">
+                <p>Welcome to BatsInfo</p>
+            </div>
+            <div class="col-xs-10 graph-content">
+                <p>'''...Over 1,100 different species of bats have been identified. This is approximately 20% of all of the mammals in the world.Bat Species Over 1,100 different species of bats have been identified. This is approximately ¼ of all of the mammals in the world. They aren’t out there trying to get tangled up in the hair of humans either...'''
+                </p>
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-12">
-                <div class="panel panel-default" style="background-color: aquamarine">
+            <div class="col-xs-10 graph">
+                <div class="panel panel-default" style="background-color: rgba(76, 71, 67, 0.12)">
                     <div class="panel-heading">
-                        Population of Bats
+                        Population of Bats in BatsInfo
                     </div>
                     <div class="panel-body">
                         <div style="margin-top: 20px;">
