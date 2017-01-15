7e7317320f4faa4fc8e8db4c5d37e6ef79cf2706
AnjaleeAbeysekera <anji.abeysekera@gmail.com>
2017-01-15 17:18:40 +0530

    background added

--- index.php
+++ index.php
@@ -37,20 +37,20 @@
     </style>
 </head>
 
-<body>
+<body style="margin: 0px;"">
 
     <a name="home"></a>
-    <div class="container-fluid" >
+    <div class="container-fluid"  >
         <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #4c4743;">
         <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-              <span class="sr-only">Toggle navigation</span>
+                <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-        </button>
-      </div>
+                </button>
+            </div>
 
         <div class="collapse navbar-collapse" id="myNavbar">
         <a class="navbar-brand" href="#"><span style="font-family: 'Chewy', cursive;
@@ -61,34 +61,42 @@
                     <li><a href="view/news/u_newst.php">News</a></li>
                     <li><a href="view/articles/article2.php">Articles</a></li>
                     <li><a href="view/forum/publicTopics.php">Forum</a></li>
-                    <li><a href="view/forum/publicTopics.php">Gallery</a></li>
+                    <li><a href="view/profiles/u_profiles.php">Gallery</a></li>
                     <li><a href="view/aboutus/aboutus.php">About Us</a></li>
                         
                 </ul>
             <form class="navbar-form  navbar-right" action="view/rltsearch/u_testing.php" method="post">
               <div class="form-group" style="padding-top:1px;">
                   <input type="text" class="form-control input-area" name='address' placeholder="Enter name here">
-              <button type="submit" class="btn btn-default">Search</button>
+              <button type="submit" class="btn btn-default">Search
+              </button>
               </div>
             </form>
             <ul class="nav navbar-nav navbar-right" style="font-size: 16px; padding-top: 1px; padding-bottom: 8px;">
            
-                    <li><a href="controller/login/page1_form.php" id="myBtn" style="font-size: 16px;"><span class="glyphicon glyphicon-user"></span> Register</a></li>
-                    <li><a href="controller/login/login.php"  style="font-size: 16px;"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="controller/login/page1_form.php" id="myBtn" style="font-size: 16px;"><span class="glyphicon glyphicon-user"></span> Register</a>
+                    </li>
+                    <li><a href="controller/login/login.php"  style="font-size: 16px;"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                    </li>
               
             </ul>
-            </div>
+        </div>
         
     </nav>
 
-    </div>
-    <div class="container" style="padding-top:100px;">
+</div>
+<div class="public-background">
+    <img src="assets/images/bat.jpg" width="100%" height="100%" >
+</div>
+    <div class="col-xs-12 body-content">
+    <div class="public-thread-content public-div-content col-xs-10">
+    <div class="container" style="padding-top:20px; padding-right: 110px; width: 800px;">
         <?php include ("assets/IncludedFiles/headerImage.php") ?>
     </div>
-
-    <div class="container-fluid" style="padding-top:30px; padding-bottom:200px;">
+    
+    <div class="container-fluid" style="padding-top:30px; padding-bottom:200px; ">
       
-        <h2><a name="articles" style="text-decoration:none;" href="../articles/article.php">Recent Articles</a></h2>
+        <h2 style="font-color:black"><a name="articles" style="text-decoration:none; " href="">Recent Articles</a></h2>
         
         <?php 
 include_once 'database/dbconnect.php';
@@ -103,7 +111,7 @@ if ($result->num_rows > 0) {
         
         echo "<div class='col-xs-4'>";
         echo "<h3>".$row['title']."</h3>";
-        //echo "<p>".$row['content']."</p>";
+        
         // strip tags to avoid breaking any html
         $string = strip_tags($row['content']);
 
@@ -125,15 +133,15 @@ if ($result->num_rows > 0) {
 }
 
 ?>
-
-    
-    <div class="push"></div>
+</div>
+ </div>
+ </div>
+ 
+ </div>
     <!--get footer -->
-    <div id="footer" class="container=fluid" >
-        <a name="contact"> <?php include ("assets/IncludedFiles/footer.php") ?> </a>
-    </div>
+<div id="footer" class="container-fluid" >
+    <a name="contact"> <?php include ("assets/IncludedFiles/footer.php") ?> </a>
+</div>
 
-  
-  
 </body>
 </html>
