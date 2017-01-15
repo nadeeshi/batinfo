0ba203b684a469691dbcdb131b30d2466a6e3467
macnmunasinghe <munasinghenuwan@yahoo.com>
2017-01-15 21:50:58 +0530

    cn26
    h

--- view/batmap/distribution_c.php
+++ view/batmap/distribution_c.php
@@ -105,7 +105,7 @@
 					<h4>OOPS!!! NO MATCH FOUND</h4>
 					<ul>
 						<li>
-							<img src="images/404.jpg" alt="404" style = "display: block; margin:auto !important;  height: 80% ; width: 100%; height:auto;"/>
+							<img src="../../assets/images/404.jpg" alt="404" style = "display: block; margin:auto !important;  height: 80% ; width: 100%; height:auto;"/>
 						</li>
 					</ul>
 				
