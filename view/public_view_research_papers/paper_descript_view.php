0e8bbda1492cbbfde4789c2a8dc08fc72e27bedb
NarthanaSathsara <ultralord550@gmail.com>
2017-01-16 12:58:14 +0530

    papper

---
+++ view/public_view_research_papers/paper_descript_view.php
@@ -0,0 +1,125 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>research papers view</title>
+
+        <link rel="stylesheet" href="../../assets/CSS/style_insert_del_edit.css"/>
+        <link rel="stylesheet" href="../../assets/CSS/headline.css"/>
+        <link rel="stylesheet" href="../../assets/CSS/insert_form_css.css">
+            
+        <link href="../../assets/CSS/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="../../assets/CSS/navbar1n2.css" rel="stylesheet" type="text/css">
+        <script src="../../assets/JS/jquary.js"></script>
+        <script src="../../assets/JS/jquery.js"></script>
+        <script src="../../assets/JS/bootstrap.js"></script>
+        <style>body{background-color: beige;}
+         h2,.lbl_cat{
+                color: white;
+
+            }
+           
+            .pdf-link{
+                 color: white;
+               background-color: coral;
+            }
+            .pdf-link:hover{
+                color: blue;
+            }
+        .form-control[disabled], fieldset[disabled] .form-control {
+    cursor: help !important;
+}
+        
+        </style>
+    </head>
+
+    <body >
+        <div class="public-background">
+            <img src="../../assets/images/bat.jpg" width="100%" height="100%" >
+        </div>
+        <div>
+            <?php include '../../assets/IncludedFiles/mainnav.php' ?>
+        </div>
+       <?php
+        require_once('../../database/dbconnect.php');
+
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+        $research_papers = $_GET['id'];
+
+        $result = mysqli_query($con, "SELECT * FROM research_papers WHERE paper_id= '$research_papers' ");
+
+        $details = mysqli_fetch_array($result);
+
+        if (!$details) {
+
+            echo "<p class='msg'>Error Occur! <p>";
+
+            echo '<br><br><a href="edit_delete_home.php"><button class="my-button">Try Again</button></a>';
+            exit();
+        }
+
+        mysqli_close($con);
+        ?>
+
+        <div class="col-sm-9 col-sm-push-2 col-xs-12 insert-form edit-form">
+             <div class="head-form">
+                    <h2>Reasearch Paper Details</h2>
+                </div>
+            <form id="form" method="post" action="">
+                <div class="form-group clearfix">
+                    <label class="lbl_cat " >Title</label>
+
+                    <input type="text"   class="form-control " value="<?php echo $details['title'] ?>"  disabled />
+
+                </div>
+
+                <div class="form-group clearfix">
+                    <label class="lbl_cat " >Author</label>
+
+                    <input type="text"  class="form-control " value="<?php echo $details['author'] ?>"  disabled />
+                </div>
+              
+
+                <div class="form-group clearfix">
+                    <label class="lbl_cat" >Description</label>
+                    <textarea class="form-control my-text" name="description" id="description" rows="4" cols="50"  disabled> <?php echo $details['description'] ?></textarea>
+
+                </div>
+
+
+                <div class="form-group clearfix">
+                    <label class="lbl_cat " >Research Area </label>
+
+                    <input  class="form-control " value="<?php echo $details['area'] ?>"  disabled/>
+
+                </div>
+
+                <div class="form-group clearfix">
+                    <label class="lbl_cat " >Research Category </label>
+
+                    <input  class="form-control " value="<?php echo $details['category'] ?>"  disabled/>
+
+                </div>
+                <div class="form-group clearfix">
+
+                    <div class="pdf-link">
+                        <a target="_blank" class="pdf-link" href="../../assets/<?php echo $details['paper'] ?> ">Click Here to View the Research Paper</a>
+                    </div>
+                </div>
+
+            </form>                
+
+        </div>
+       
+        <div class=" col-xs-12 ">
+            <?php include "../../assets/IncludedFiles/footer.php" ?>
+        </div>
+    </body>
+</html>
--- view/public_view_research_papers/paper_view.php
+++ view/public_view_research_papers/paper_view.php
@@ -1,128 +1,153 @@
-<?php 
-require_once ("../../database/connection.php");
+<?php
+require_once ("../../database/dbconnect.php");
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>addThread</title>
-	<link href="../../assets/CSS/bootstrap.css" rel="stylesheet" type="text/css">
-	
-	<link href="../../assets/CSS/navbar1n2.css" rel="stylesheet" type="text/css">
-	<script src="../../assets/JS/jquery.js"></script>
-    <script src="../../assets/JS/bootstrap.js"></script>
-     <link rel="stylesheet" href="../../assets/CSS/style_insert_del_edit.css"/>
-         <link rel="stylesheet" href="../../assets/CSS/research_papers_view.css"/>
+    <head>
+        <title>view research papers</title>
+
+
+        <link rel="stylesheet" href="../../assets/CSS/style_insert_del_edit.css"/>
+        <link rel="stylesheet" href="../../assets/CSS/research_papers_view.css"/>
         <link rel="stylesheet" href="../../assets/CSS/headline.css"/>
+
         <link rel="stylesheet" href="../../assets/CSS/bats_view.css"/>
         <link rel="stylesheet" href="../../assets/CSS/edit_del_page.css"/>
         <link rel="stylesheet" href="../../assets/CSS/insert_form_css.css">
+
+
         <link href="../../assets/CSS/bootstrap.css" rel="stylesheet" type="text/css">
         <link href="../../assets/CSS/navbar1n2.css" rel="stylesheet" type="text/css">
         <script src="../../assets/JS/jquary.js"></script>
         <script src="../../assets/JS/jquery.js"></script>
         <script src="../../assets/JS/bootstrap.js"></script>
-  	<style type="text/css">
-	    table tr:nth-child(even) {
-	        background-color: #e1e1d0;
-	    }
-	    table tr:nth-child(odd) {
-	        background-color: white;
-	    }
-        .head-view{
-               margin-bottom: -65px;
-    margin-top: 69px;
-    margin-left: 29% !important;
-            
-        }
-        body{
-            align-content: center !important;
-        }
-	</style>
-</head>
-<body>
-	<div class="public-background">
-		<img src="../../assets/images/bat.jpg" width="100%" height="100%" >
-	</div>
-	<div>
-		<div>
-			<?php include '../../assets/includedFiles/mainnav.php' ?>
-		</div>
-        <div class="col-sm-8 col-sm-push-2 col-xs-12 insert-form body-content">
-		
-<div class="head-view">
-                    <h2 >Reasearch Paper Details</h2>
+        <style type="text/css">
+            table tr:nth-child(even) {
+                background-color: #e1e1d0;
+            }
+            table tr:nth-child(odd) {
+                background-color: white;
+            }
+            .head-view{
+                margin-bottom: -65px;
+                margin-top: 69px;
+                margin-left: 29% !important;
+
+            }
+            .th_sname{
+
+            }
+            body{
+                align-content: center !important;
+            }
+            h2{
+                color: white;
+
+            }
+            td{
+                align-content: center !important;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="public-background">
+            <img src="../../assets/images/bat.jpg" width="100%" height="100%" >
+        </div>
+        <div>
+            <div>
+                <?php include '../../assets/includedFiles/mainnav.php' ?>
             </div>
-            <div class="table" >
+            <div class="col-sm-8 col-sm-push-2 col-xs-12 insert-form body-content">
+
+                <div class="head-view">
+                    <h2 >Reasearch Papers Categories</h2>
+                </div>
+                <div class="table" >
 
 
-                <?php
-                require_once('../../database/dbconnect.php');
-                if (mysqli_connect_errno()) {
-                    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                }
+                    <?php
+                    require_once('../../database/dbconnect.php');
+                    if (mysqli_connect_errno()) {
+                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                    }
 
-                $sql = "SELECT * FROM research_papers WHERE del_bit='0'";
-                $result = mysqli_query($con, $sql);
-                ?>
-                <table >
-                    <thead>
+                
+                $diet= "Diet & Feed";
+                $habits="Habitats";
+                $anatomy="Anatomy";
+                $echolocation="Echolocation";
+                $behaviour="Behaviour";
+                $conservation="Conservation";
+                $others="Others";
+               
+                
+                
+                
+                
+                    ?>
+                    <table style="width:100%">
                         <tr>
-                            <td class='td th_option-head th_option lbl head'></td>
+                            <th class="lbl">CATEGORY</th>
 
-                                <td class='td th_sname lbl head'>Category</td>
-                           
-                                <td class='td th_option lbl head'>Action</td>
-                                
-                             
+                            <th class="lbl">ACTION</th>
+                        </tr>
+                        <tr>
+                            <td>Diet & Feed</td>
+                           <?php echo '<td ><a class="link" href="view_cat_papers.php?category=' . $diet . '" onclick="return myFunction_update();">view</a></td>';?>
 
                         </tr>
-                    </thead>
+                        <tr>
+                            <td>Habitats</td>
+                             <?php echo '<td ><a class="link" href="view_cat_papers.php?category=' . $habits . '" onclick="return myFunction_update();">view</a></td>';?>
+                        </tr>
+                        <tr>
+                            <td>Anatomy</td>
+                           <?php echo '<td ><a class="link" href="view_cat_papers.php?category=' . $anatomy . '" onclick="return myFunction_update();">view</a></td>';?>
 
-                    <tbody>
+                        </tr>
+                        <tr>
+                            <td>Echolocation</td>
+                            <?php echo '<td ><a class="link" href="view_cat_papers.php?category=' . $echolocation . '" onclick="return myFunction_update();">view</a></td>';?>
+                        </tr>
+                        <tr>
+                            <td>Behaviour</td>
+                             <?php echo '<td ><a class="link" href="view_cat_papers.php?category=' . $behaviour . '" onclick="return myFunction_update();">view</a></td>';?>
 
-                <?php
-                $i = 1;
-                while ($row = mysqli_fetch_array($result)) {
-                    echo '<tr id="tr_data">';
-                    echo '<td class="td_data num lbl">' . $i . '</td>';
-                    echo '<td class="td_data lbl">'.$row['category'] . '</td>'; 
-                   
-                    $i = $i + 1;
+                        </tr>
+                        <tr>
+                            <td>Conservation</td>
+                            <?php echo '<td ><a class="link" href="view_cat_papers.php?category=' . $conservation . '" onclick="return myFunction_update();">view</a></td>';?>
+
+                        </tr>
+                        <tr>
+                            <td>Others</td>
+                             <?php echo '<td ><a class="link" href="view_cat_papers.php?category=' . $others . '" onclick="return myFunction_update();">view</a></td>';?>
+
+                        </tr>
+                    </table>
+
+                </div>
 
 
 
-                    echo '<td class="td_data td_data_op lbl"><a class="link" href="category_view.php?category=' . $row['category'] . '" ">view</a></td>';
 
-                    echo '</tr>';
-                }
-                        
-                        
-                mysqli_close($con);
-                ?>
-                    </tbody>
-                </table>
-              
             </div>
-            
-            
-            
-            
-		</div>
-	</div>
-
-	
+        </div>
 
 
-	<!-- start footer -->
-
-	<div>
-	    <div class="col-xs-12 ">
-	      <?php include "../../assets/includedFiles/footer.php" ?>
-	    </div>  
-  	</div>
 
 
-	<!-- end of footer -->
-</body>
+        <!-- start footer -->
+
+        <div>
+            <div class="col-xs-12 ">
+                <?php include "../../assets/includedFiles/footer.php" ?>
+            </div>  
+        </div>
+
+
+        <!-- end of footer -->
+    </body>
 </html>
